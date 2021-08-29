2621cc15fcc9975a50e4dec901b481d8dd698b04
rodrigospimentacwb <rodrigospimenta@gmail.com>
2021-08-29 19:49:12 -0300

    15. Testando a primeira classe de domínio -> CategoriaEntity

--- src/main/java/com/pepper/edu/springvendas/controller/CategoriaController.java
+++ src/main/java/com/pepper/edu/springvendas/controller/CategoriaController.java
@@ -1,16 +1,22 @@
 package com.pepper.edu.springvendas.controller;
 
+import com.pepper.edu.springvendas.model.CategoriaEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/categorias")
-public class Categoria {
+public class CategoriaController {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar(){
-        return "REST está funcionando";
+    public List<CategoriaEntity> listar(){
+
+        CategoriaEntity cat1 = new CategoriaEntity(1,"Informática");
+        CategoriaEntity cat2 = new CategoriaEntity(2,"Escritório");
+        return List.of(cat1,cat2);
     }
 
 }
---
+++ src/main/java/com/pepper/edu/springvendas/model/CategoriaEntity.java
@@ -0,0 +1,48 @@
+package com.pepper.edu.springvendas.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class CategoriaEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private Integer id;
+    private String nome;
+
+    public CategoriaEntity(){
+    }
+
+    public CategoriaEntity(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CategoriaEntity that = (CategoriaEntity) o;
+        return Objects.equals(id, that.id) && Objects.equals(nome, that.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome);
+    }
+}

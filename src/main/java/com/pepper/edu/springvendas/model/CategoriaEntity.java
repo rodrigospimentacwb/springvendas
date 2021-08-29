1139187d0c2e8ddd8c9fcec55a0db80acb15e7ba
rodrigospimentacwb <rodrigospimenta@gmail.com>
2021-08-29 20:19:36 -0300

    16. Banco de dados H2 e criação automática da base de dados -> configurado H2, Classe de entidade e importação do JPA no pom.xml

--- src/main/java/com/pepper/edu/springvendas/model/CategoriaEntity.java
+++ src/main/java/com/pepper/edu/springvendas/model/CategoriaEntity.java
@@ -1,11 +1,21 @@
 package com.pepper.edu.springvendas.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "Categoria")
 public class CategoriaEntity implements Serializable {
 
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 
